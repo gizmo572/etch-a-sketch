b85cd7008f5774a059830b2d8886a238205a4ead
Nick Cushman <cushmann572@gmail.com>
2023-06-02 09:30:14 -0700

    add input form to index.html, remove flexBasis and paddingBottom styles from css and rather create them within createGrid func of script.js. edit createGrid to accept number and create custom grids of equal overall size

--- script.js
+++ script.js
@@ -1,19 +1,28 @@
 // const container = document.getElementsByClassName('container');
 
 
-function createGrid() {
+function createGrid(num) {
+    let percentage = 100 / num;
+
     let count = 0;
-    while (count < 16 * 16) {
+    while (count < num**2) {
         count++;
         const newDiv = document.createElement('div');
 
         newDiv.classList.add('square');
-
+        newDiv.style.flexBasis = percentage + '%';
+        newDiv.style.paddingBottom = percentage + '%';
         document.querySelector('.container').appendChild(newDiv);
+
     }
 }
 
-createGrid();
+createGrid(64)
+
+// document.getElementById('submit').addEventListener('click', () => {
+//     createGrid(document.getElementById('size').value);
+//     console.log(size)
+// })
 
 document.querySelectorAll('.square').forEach((div) => {
     div.addEventListener('mouseover', () => {
